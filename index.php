be07467113ef8be17e98d342b7f30d17cc7e77ef
Neciu Dan <dan@adoreme.com>
2018-07-27 11:49:29 +0300

    working on site

--- index.php
+++ index.php
@@ -55,11 +55,24 @@
         </nav>
     </header>
     <div class="col-md-12">
-        <h2>Hello World</h2>
+        <div id="slider">
+            <div class="overlay red"></div>
+            <div class="overlay yellow"></div>
+            <div class="overlay blue"></div>
+            <a href="#" class="control_next">></a>
+            <a href="#" class="control_prev"><</a>
+            <ul>
+                <li>SLIDE 1</li>
+                <li>SLIDE 2</li>
+                <li>SLIDE 3</li>
+                <li>SLIDE 4</li>
+            </ul>
+        </div>
     </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.1.0.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="scrips.js"></script>
 </body>
 </html>
