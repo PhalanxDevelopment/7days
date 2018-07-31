7714abef7d5208e427a75c856c40d7b69f747396
Neciu Dan <cst2989@gmail.com>
2018-07-31 14:31:27 +0300

    version 1

--- index.php
+++ index.php
@@ -1,78 +1,219 @@
 <html>
-<head>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
-    <title>7 zile de unire</title>
-</head>
-<body>
-<div class="container">
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">
-                <img src="http://www.elmhursthistory.org/ImageRepository/Document?documentID=559" alt="">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse flex-column" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto ">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-
-                </ul>
-                <br>
-                <form class="form-inline flex-row my-2 my-lg-0 ml-auto">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
+   <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;subset=latin-ext" rel="stylesheet">
+      <link rel="stylesheet" href="style.css">
+      <title>7 zile de unire</title>
+   </head>
+   <body>
+      <div class="fixed_buttons">
+         <div class="buts">
+            <a class="but big" href="   "><img src="img/back_to_top.png"></a>
+            <a class="but" href="   "><img src="img/facebook.png"></a>
+            <a class="but" href="   "><img src="img/facebook.png"></a>
+            <a class="but" href="   "><img src="img/facebook.png"></a>
+            <a class="but" href="   "><img src="img/facebook.png"></a>
+         </div>
+      </div>
+      <div class="container">
+         <div class="row">
+            <header>
+               <nav class="navbar navbar-expand-lg navbar-light">
+                  <a class="navbar-brand" href="#">
+                  <img src="img/logo.jpg" alt="">
+                  </a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse flex-column" id="navbarSupportedContent">
+                     <ul class="navbar-nav ml-auto ">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="#">Pagina Principala <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="pagina.html">Festival</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="pagina.html">Artele Unirii</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="pagina.html">Concursuri</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="pagina.html">Contact</a>
+                        </li>
+                     </ul>
+                     <br>
+                     <form class="form-inline flex-row my-2 my-lg-0 ml-auto">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cauta</button>
+                     </form>
+                  </div>
+               </nav>
+            </header>
+         </div>
+         <div class="row">
+            <div class="col-sm">
+               <div id="slider">
+                  <div class="overlay red"></div>
+                  <div class="overlay yellow"></div>
+                  <div class="overlay blue"></div>
+                  <a href="#" class="control_next">></a>
+                  <a href="#" class="control_prev"><</a>
+                  <ul>
+                     <li>SLIDE 1</li>
+                     <li>SLIDE 2</li>
+                     <li>SLIDE 3</li>
+                     <li>SLIDE 4</li>
+                  </ul>
+               </div>
             </div>
-        </nav>
-    </header>
-    <div class="col-md-12">
-        <div id="slider">
-            <div class="overlay red"></div>
-            <div class="overlay yellow"></div>
-            <div class="overlay blue"></div>
-            <a href="#" class="control_next">></a>
-            <a href="#" class="control_prev"><</a>
-            <ul>
-                <li>SLIDE 1</li>
-                <li>SLIDE 2</li>
-                <li>SLIDE 3</li>
-                <li>SLIDE 4</li>
-            </ul>
+         </div>
+         <div class="row">
+            <div class="col-6">
+               <div class="day_box">
+                  <div class="day_image">
+                     <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                  </div>
+                  <h2>Ziua I</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
+                  <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+               </div>
+            </div>
+            <div class="col-6">
+               <div class="row">
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2 class="red">Ziua II</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2 class="yellow">Ziua III</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2>Ziua IV</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+               </div>
+               <div class="row">
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2 class="red">Ziua V</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2 class="yellow">Ziua VI</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+                  <div class="col">
+                     <div class="day_box">
+                        <div class="day_image">
+                           <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                        </div>
+                        <h2>Ziua VII</h2>
+                        <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="bgimg-3">
+         <div class="caption ">
+            <span  class="container" style="background-color:transparent;font-size:25px;color: #f7f7f7;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la</span>
+         </div>
+      </div>
+      <div class="container">
+        <div class="concursuri">
+            <div class="row">
+                <div class="col-6">
+                    <img src="https://static1.squarespace.com/static/55894a8ce4b07616419003f9/t/59426f66be6594e89ba78837/1497526140006/IMG_0167.jpg?format=2500w">
+                </div>
+                <div class="col-6">
+                    <h2>Concursuri</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la</p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
+                    </p>
+                     <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco la
+                    </p>
+                     <a class="btn btn-outline-success my-2 my-sm-0" >Afla mai multe...</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<script src="https://code.jquery.com/jquery-3.1.0.js" ></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="scrips.js"></script>
-</body>
+      </div>
+      <div class="map">
+        
+        <div class="container">
+            <h2>Locatia Noastra</h2>
+             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2848.4104695144306!2d26.110191315720886!3d44.44525380886962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1ff4a9fcdf761%3A0x7757ab4bdad03c68!2sPiata+Gemeni!5e0!3m2!1sen!2sro!4v1533032250621" width="1140" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+       
+      </div>
+      <footer>
+         <div class="container">
+            <div class="row">
+               <div class="col-sm">
+                  <a  href="#">
+                  <img src="img/logo.jpg" alt="">
+                  </a>
+                  <br><br><br>
+               </div>
+               <div class="col-sm">
+                  <h2>CONTACT</h2>
+                  Adresa lorem Ipsum 123 <br>
+                  Lorem Usda 120, iasd <br>
+                  Kiren Upsum Ispumn <br>
+                  Telefon: 021 / 333 / 123
+               </div>
+               <div class="col-sm">
+                  <h2>HARTA WEBSITE</h2>
+                  <ul>
+                     <li>
+                        <a href="">PAGINA PRINCIPALA</a>
+                     </li>
+                     <li>
+                        <a href="">FESTIVAL</a>
+                     </li>
+                     <li>
+                        <a href="">ARTELE UNIRII</a>
+                     </li>
+                     <li>
+                        <a href="">CONTACT</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.1.0.js" ></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <script src="scrips.js"></script>
+   </body>
 </html>
